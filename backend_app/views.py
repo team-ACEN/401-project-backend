ed031b81c99f0f9bd83ef29b898e22116636a48a
CODY DE LA TORRE <cdelatorre414@gmail.com>
2023-02-22 12:20:30 -0800

    added functions

--- backend_app/views.py
+++ backend_app/views.py
@@ -23,6 +23,7 @@ def trim_genres(data):
     trimmed_data = [d for d in data if d["name"] in genres_to_keep]
     return trimmed_data
 
+#Have to figure out where/how we are getting the search request from the frontend
 def search(request):
     term='Titanic'
     url = f'https://partner-api.reelgood.com/v1.0/content/search?term={term}&all_services=true&content_type=Both'
@@ -46,9 +47,7 @@ def get_services(request):
     if response.status_code == 200:
         data = json.loads(response.content)
         data2 = {"services": trim_services(data)}
-        # data2 = trim_services(data)
         return JsonResponse(data2, safe=False)
-        # return HttpResponse(response)
     return JsonResponse({"error": f"Failed to retrieve genres from Reelgood API due to {response.status_code}"})
 
 def trim_services(data):
@@ -56,25 +55,28 @@ def trim_services(data):
     trimmed_data = [d for d in data if any(service["display_name"] in services_to_keep for service in d["services"])]
     return trimmed_data
 
-    
-    
+#Have to figure where/how we are pulling service ids
+def get_popular_based_on_service(request):
+    service_ids = [1, 2, 4]  # Example list of service ids
+    service_ids_str = ''.join(f'&service={id}' for id in service_ids)
+    url = f'https://partner-api.reelgood.com/v1.0/content/browse/Both?order_by=Popular&all_services=false{service_ids_str}'
+    headers = {
+        'Accept': 'text/plain',
+        'x-api-key': os.getenv('API_KEY')
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = json.loads(response.content)
+        return JsonResponse(data, safe=False)
+    return JsonResponse({"error": f"Failed to retrieve genres from Reelgood API due to {response.status_code}"})
 
-# class ShowList(ListCreateAPIView):
-#     queryset = Show.objects.all()
-#     serializer_class = ShowSerializer
-
-
-# class ShowDetail(RetrieveUpdateDestroyAPIView):
-#     # permission_classes = (IsOwnerOrReadOnly,)
-#     queryset = Show.objects.all()
-#     serializer_class = ShowSerializer
-
-# class MovieList(ListCreateAPIView):
-#     queryset = Movie.objects.all()
-#     serializer_class = MovieSerializer
-
-
-# class MovieDetail(RetrieveUpdateDestroyAPIView):
-#     # permission_classes = (IsOwnerOrReadOnly,)
-#     queryset = Movie.objects.all()
-#     serializer_class = MovieSerializer
+# Netflix id = 1
+# Prime id = 2
+# Hulu id = 4
+# crunchyroll id = 12
+# Prime(free) id = 78
+# crunchyroll(free) id = 92
+# Hulu(free)  id = 115
+# Disney+ id = 353 
+# Peacock(free) = 367
+# Paramount+ = 375
